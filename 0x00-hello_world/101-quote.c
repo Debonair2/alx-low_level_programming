12d180724d07567c622c39e4ba535a2d31e15a27
Dare Blessing <dareblessing12@gmail.com>
2022-09-08 20:40:59 -0700

    hello

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 
 /**
- * main - prints out he last part of a quote in the standard error
+ * main - prints out he last part of a quote in the standard error.
  *
- * Return: Always 1 (Success)
+ * Return: 1 if success.
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59)
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
 			
 	return (1);
 }

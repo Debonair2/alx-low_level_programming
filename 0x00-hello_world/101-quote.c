7251d24b512c1b58556583918077d569d31602f7
Dare Blessing <dareblessing12@gmail.com>
2022-09-08 21:11:37 -0700

    hello

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,12 @@
 
 /**
  * main -Prints out the last oart of a quote in the standartd error.
- * Return 1 if success.
+ *
+ * Return: 1 if success.
  */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	
 	return (1);
 }

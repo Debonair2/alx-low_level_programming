02bb436228926c1c9a2e1e929077389c6480cdfd
Dare Blessing <dareblessing12@gmail.com>
2022-09-13 07:54:34 -0700

    hello

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,15 +1,20 @@
 #include "main.h"
 
 /**
- * print_alphabet - Check description
- * Description: It prints the alphabet in lowercase fallowed by a new line
- * Return: Nothing.
+ * _abs - computes the absolute value of an integer
+ *@n: integer whose absolute value to find
+ *
+ * Return: int
  */
-void print_alphabet(void)
-{
-	char i;
 
-	for (i = 'a'; i <= 'z'; i++)
-		_putchar(i);
-	_putchar('\n');
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	else
+	{
+		return (n);
+	}
 }

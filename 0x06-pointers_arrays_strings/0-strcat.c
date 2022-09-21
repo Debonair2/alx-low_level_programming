3f9bbbae680fcce3bc258b64408467901d14a7cd
Dare Blessing <dareblessing12@gmail.com>
2022-09-21 10:15:53 -0700

    hello

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
 /**
+ *_strcat - function to concatenate two strings and return a new string
  *
  * @dest: string 1
  * @src: string 2
@@ -23,4 +24,3 @@ char *_strcat(char *dest, char *src)
 	p = dest;
 	return (p);
 }
-

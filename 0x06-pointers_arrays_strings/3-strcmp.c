dece9ead5c046fd051169929e4d6d55adb3c758d
Dare Blessing <dareblessing12@gmail.com>
2022-09-21 10:24:16 -0700

    hello

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,7 +4,7 @@
  * _strcmp - function to copy string
  *
  * @s1: first string to compare
- * s2: second string to compare
+ * @s2: second string to compare
  * Return: int
  */
 int _strcmp(char *s1, char *s2)

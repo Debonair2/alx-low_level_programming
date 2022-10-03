56f670b321ea614dd73f2c8d07aac48a289778c9
Dare Blessing <dareblessing12@gmail.com>
2022-10-03 13:04:07 -0700

    hello

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,4 +1,4 @@
-#ifndef MAIN_H
+fndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
 
@@ -16,11 +16,11 @@ char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
-cha *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif /* MAIN */
+#endif /* MAIN_H */
